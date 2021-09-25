0fb3d0f7c2bc91e7236a73e753f6938837bcb6dd
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2021-09-25 18:23:24 +0300

    Update UT

--- src/test/java/org/wcdevs/blog/cdk/InputParametersTest.java
+++ src/test/java/org/wcdevs/blog/cdk/InputParametersTest.java
@@ -1,15 +1,17 @@
 package org.wcdevs.blog.cdk;
 
+import java.security.SecureRandom;
+import java.util.Random;
+import java.util.UUID;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import software.amazon.awscdk.core.RemovalPolicy;
 import software.amazon.awscdk.services.ecr.TagMutability;
-import java.security.SecureRandom;
-import java.util.Random;
-import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class InputParametersTest {
+  private Random random = new SecureRandom();
+
   private static String randomString() {
     return UUID.randomUUID().toString();
   }
@@ -24,36 +26,45 @@ class InputParametersTest {
     assertEquals(repositoryName, actual.getRepositoryName());
     assertEquals(accountId, actual.getAccountId());
     assertEquals(DockerRepository.InputParameters.DEFAULT_MAX_IMAGE_COUNT,
-                            actual.getMaxImageCount());
+                 actual.getMaxImageCount());
     assertEquals(DockerRepository.InputParameters.DEFAULT_RETAIN_POLICY,
-                            actual.isRetainRegistryOnDelete());
+                 actual.isRetainRegistryOnDelete());
     assertEquals(DockerRepository.InputParameters.DEFAULT_INMUTABLE_TAGS,
-                            actual.isInmutableTags());
+                 actual.isInmutableTags());
     assertEquals(TagMutability.IMMUTABLE, actual.tagMutability());
     assertEquals(RemovalPolicy.RETAIN, actual.removalPolicy());
   }
 
   @Test
-  void allArgsConstructor() {
-    Random random = new SecureRandom();
+  void allArgsConstructorRetainImageInmutableTags() {
+    testAllArgsConstructor(random.nextInt(),
+                           true, RemovalPolicy.RETAIN,
+                           true, TagMutability.IMMUTABLE);
+  }
+
+  @Test
+  void allArgsConstructorDestroyImageMutableTags() {
+    testAllArgsConstructor(random.nextInt(),
+                           false, RemovalPolicy.DESTROY,
+                           false, TagMutability.MUTABLE);
+  }
+
+  void testAllArgsConstructor(int expectedMaxImageCount, boolean expectedRetainRegistry,
+                              RemovalPolicy expectedRemovalPolicy, boolean expectedInmutableTags,
+                              TagMutability expectedTagMutability) {
     String repositoryName = randomString();
     String accountId = randomString();
-    int maxImageCount = random.nextInt();
-    boolean retainRegistry = random.nextBoolean();
-    boolean inmutableTags = random.nextBoolean();
 
     DockerRepository.InputParameters actual
-        = new DockerRepository.InputParameters(repositoryName, accountId, maxImageCount,
-                                               retainRegistry, inmutableTags);
+        = new DockerRepository.InputParameters(repositoryName, accountId, expectedMaxImageCount,
+                                               expectedRetainRegistry, expectedInmutableTags);
     Assertions.assertNotNull(actual);
     assertEquals(repositoryName, actual.getRepositoryName());
     assertEquals(accountId, actual.getAccountId());
-    assertEquals(maxImageCount, actual.getMaxImageCount());
-    assertEquals(retainRegistry, actual.isRetainRegistryOnDelete());
-    assertEquals(inmutableTags, actual.isInmutableTags());
-    assertEquals(inmutableTags ? TagMutability.IMMUTABLE : TagMutability.MUTABLE,
-                 actual.tagMutability());
-    assertEquals(retainRegistry ? RemovalPolicy.RETAIN : RemovalPolicy.DESTROY,
-                 actual.removalPolicy());
+    assertEquals(expectedMaxImageCount, actual.getMaxImageCount());
+    assertEquals(expectedRetainRegistry, actual.isRetainRegistryOnDelete());
+    assertEquals(expectedInmutableTags, actual.isInmutableTags());
+    assertEquals(expectedTagMutability, actual.tagMutability());
+    assertEquals(expectedRemovalPolicy, actual.removalPolicy());
   }
 }
