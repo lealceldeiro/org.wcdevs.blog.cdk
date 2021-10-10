cca16f15bf92aba89f3b35f8935a429933d7ca19
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2021-10-10 11:40:12 +0300

    Update Util
    - Add utility method `sanitize`

--- src/main/java/org/wcdevs/blog/cdk/Util.java
+++ src/main/java/org/wcdevs/blog/cdk/Util.java
@@ -5,9 +5,12 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 final class Util {
-  private Util() {}
+  static final String NON_ALPHANUMERIC_VALUES = "[^a-zA-Z0-9-]";
 
-  static String string(Object ... values) {
+  private Util() {
+  }
+
+  static String string(Object... values) {
     return Arrays.stream(values)
                  .filter(Objects::nonNull)
                  .map(Object::toString)
@@ -19,5 +22,17 @@ final class Util {
                  .filter(Objects::nonNull)
                  .map(Object::toString)
                  .collect(Collectors.joining(joiner));
+  }
+
+  /**
+   * Returns the provided string with the non-alphanumeric characters striped. Useful when some AWS
+   * resources don't cope with them in resource names.
+   *
+   * @param value String value to be sanitized.
+   *
+   * @return The sanitized String.
+   */
+  static String sanitize(String value) {
+    return Objects.requireNonNull(value).replaceAll(NON_ALPHANUMERIC_VALUES, "");
   }
 }
